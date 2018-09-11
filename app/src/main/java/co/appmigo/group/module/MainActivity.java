e773ba817199dc7f02686bc9a4d929f6134d7c88
Frerit <freritmg12@gmail.com>
2018-09-11 16:54:44 -0500

    Commit Integracion con MapBox

--- app/src/main/java/co/appmigo/group/module/MainActivity.java
+++ app/src/main/java/co/appmigo/group/module/MainActivity.java
@@ -5,22 +5,47 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapFragment;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.MapboxMapOptions;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.SupportMapFragment;
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode;
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import co.appmigo.group.R;
 import co.appmigo.group.module.explore.fragment.ExploreAlertFragment;
 import co.appmigo.group.module.maps.fragment.MapsFragment;
 import co.appmigo.group.module.notification.fragment.NotificationFragment;
 
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
+import java.util.List;
 
 
+public class MainActivity extends AppCompatActivity implements MapFragment.OnMapViewReadyCallback, PermissionsListener {
+
+    private MapView mapView;
+    private MapboxMap mapboxMap;
+    private FrameLayout contentLayout;
+    private SupportMapFragment mapFragment;
+    private FragmentTransaction transaction;
+    private LocationLayerPlugin locationLayerPlugin;
+    private PermissionsManager permissionsManager;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -28,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    createHomeFragment();
+                    createHomeFragment(null);
                     return true;
                 case R.id.navigation_dashboard:
                     createExplorerFragment();
@@ -45,15 +70,44 @@ public class MainActivity extends AppCompatActivity {
     };
 
     private void createnotificationFragment() {
+        mapView.setVisibility(View.GONE);
         NotificationFragment notifrag = new NotificationFragment();
         FragmentManager chageExplore = getSupportFragmentManager();
         chageExplore.beginTransaction().replace(R.id.contentLayout, notifrag).commit();
     }
 
-    private void createHomeFragment() {
+    private void createHomeFragment(Bundle savedInstanceState) {
         MapsFragment mapsFragment = new MapsFragment();
-        FragmentManager changeMaps = getSupportFragmentManager();
-        changeMaps.beginTransaction().replace(R.id.contentLayout, mapsFragment).commit();
+
+        FragmentManager changeMap = getSupportFragmentManager();
+        changeMap.beginTransaction().replace(R.id.contentLayout, mapsFragment).commit();
+        SupportMapFragment mapFragment;
+        if (savedInstanceState == null) {
+
+            final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            LatLng patagonia = new LatLng(6.24962, -75.5695);
+
+            MapboxMapOptions options = new MapboxMapOptions();
+            options.styleUrl(Style.LIGHT);
+            options.camera(new CameraPosition.Builder()
+                    .target(patagonia)
+                    .zoom(20)
+                    .build());
+
+            mapFragment = SupportMapFragment.newInstance(options);
+            transaction.add(R.id.contentLayout, mapFragment, "com.mapbox.map");
+            transaction.commit();
+        } else {
+            mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentByTag("com.mapbox.map");
+        }
+
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                MainActivity.this.mapboxMap = mapboxMap;
+                enableLocationPlugin();
+            }
+        });
     }
 
     private void createSettingFragment() {
@@ -61,6 +115,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void createExplorerFragment() {
+        mapView.setVisibility(View.GONE);
         ExploreAlertFragment exploreFragment = new ExploreAlertFragment();
         FragmentManager changeExplorer = getSupportFragmentManager();
         changeExplorer.beginTransaction().replace(R.id.contentLayout, exploreFragment).commit();
@@ -69,12 +124,63 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        Mapbox.getInstance(this, getString(R.string.acess_token) );
+        mapView = findViewById(R.id.mapMain);
+        createHomeFragment(savedInstanceState);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    @Override
+    public void onMapViewReady(MapView mapView) {
+        this.mapView = mapView;
+    }
 
+    @SuppressWarnings( {"MissingPermission"})
+    private void enableLocationPlugin() {
+
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+
+            locationLayerPlugin = new LocationLayerPlugin(mapView, mapboxMap);
+            locationLayerPlugin.setCameraMode(CameraMode.TRACKING);
+            locationLayerPlugin.setRenderMode(RenderMode.NORMAL);
+            getLifecycle().addObserver(locationLayerPlugin);
+        } else {
+
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+        getLifecycle().addObserver(locationLayerPlugin);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+        Toast.makeText(this, "Necesitamos unos permisos", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            enableLocationPlugin();
+        } else {
+            Toast.makeText(this, "Autorizado" , Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!PermissionsManager.areLocationPermissionsGranted(this)) {
+            getLifecycle().removeObserver(locationLayerPlugin);
+        }
+    }
 }
--- app/src/main/java/co/appmigo/group/module/maps/fragment/MapsFragment.java
+++ app/src/main/java/co/appmigo/group/module/maps/fragment/MapsFragment.java
@@ -1,92 +1,28 @@
 package co.appmigo.group.module.maps.fragment;
 
-import android.Manifest;
-import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.fragment.app.Fragment;
 import co.appmigo.group.R;
-import co.appmigo.group.common.Localization;
 import co.appmigo.group.module.MainActivity;
 
-import android.provider.Settings;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.Circle;
-import com.google.android.gms.maps.model.CircleOptions;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MapStyleOptions;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-import java.util.List;
 
 
-public class MapsFragment extends Fragment implements OnMapReadyCallback {
+public class MapsFragment extends Fragment  {
 
     private MainActivity mainActivity;
-    MapView mapView;
-    private GoogleMap mMap;
-    private Circle circle;
-
-    List<Localization> locations;
-    LocationManager locationManager;
-    Localization localization;
-
-    Bitmap iconBit;
-    private int drawerIcon;
-    private int colorStroke;
-    private int colorFill;
-
-    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
-
-
     public MapsFragment() { }
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityCompat.requestPermissions((Activity) getContext(), new String[] {
-                Manifest.permission.ACCESS_FINE_LOCATION
-        }, 1);
-        locationManager = (LocationManager) getContext().getSystemService( Context.LOCATION_SERVICE);
-    }
 
-    // Cuando la vista esta gardaca con los elementos soporta el MAPA
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapView);
-        mapFragment.getMapAsync(this);
     }
 
     @Override
@@ -116,160 +52,6 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
         }
-    }
-
-    private boolean isLocationEnabled() {
-        boolean enableOrDis = locationManager.isProviderEnabled( LocationManager.GPS_PROVIDER );
-        boolean enOrdis =  locationManager.isProviderEnabled( LocationManager.NETWORK_PROVIDER );
-        Log.d( "Com ---- ", String.valueOf( enableOrDis ) );
-        return  (enableOrDis || enOrdis);
-
-    }
-    private boolean checkLocation() {
-        if (!isLocationEnabled())
-            showAlert();
-        return isLocationEnabled();
-    }
-
-
-    private void comenzarLocalizacion() {
-        LocationManager locationManager;
-        String context = Context.LOCATION_SERVICE;
-        locationManager = (LocationManager) getContext().getSystemService( context );
-
-        Criteria crta = new Criteria();
-        crta.setAccuracy( Criteria.ACCURACY_FINE );
-        crta.setAltitudeRequired( false );
-        crta.setBearingRequired( false );
-        crta.setCostAllowed( true );
-        crta.setPowerRequirement( Criteria.POWER_LOW );
-        String provider = locationManager.getBestProvider( crta, true );
-
-        if (ActivityCompat.checkSelfPermission( getContext(), Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission( getContext(), Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        Location location = locationManager.getLastKnownLocation( provider );
-        updateWithNewLocation(location);
-
-        locationManager.requestLocationUpdates(provider, 15000, 0,
-                locationListener);
-    }
-
-    private final LocationListener locationListener = new LocationListener() {
-        @Override
-        public void onLocationChanged(Location location) {
-            updateWithNewLocation(location);
-        }
-        @Override
-        public void onProviderDisabled(String provider) {
-            updateWithNewLocation(null);
-        }
-        @Override
-        public void onProviderEnabled(String provider) { }
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) { }
-
-    };
-    private void updateWithNewLocation(Location location) {
-
-        if (location != null) {
-            localization = new Localization("Actual",location.getLatitude(),location.getLongitude());
-            Log.e("LOCATION", "your Current Position is : " + localization.toNameString() );
-        }
-    }
-
-    private void setUpMap() {
-        checkLocation();
-
-        if (ActivityCompat.checkSelfPermission( getContext(), Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission( getContext(), Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-
-        try {
-            boolean success =  mMap.setMapStyle(  MapStyleOptions.loadRawResourceStyle(getActivity(), R.raw.style_appmigo) );
-            if (!success)  Log.e("MapsActivityRaw", "Style parsing failed.");
-        } catch (Resources.NotFoundException e) {
-            Log.e("MapsActivityRaw", "Can't find style.", e);
-        }
-
-        mMap.setMyLocationEnabled( true );
-        mMap.setMapType( GoogleMap.MAP_TYPE_NORMAL );
-        comenzarLocalizacion();
-        final LatLng position = new LatLng(localization.getLatitude(), localization.getLongitude());
-        mMap.moveCamera( CameraUpdateFactory.newLatLngZoom( position, 18 ) );
-        mMap.getUiSettings().setCompassEnabled( true );
-
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        this.mMap = googleMap;
-        setUpMap();
-        colorStroke = Color.argb( 80,210,180,222 );
-        colorFill = Color.argb( 100,232,218,239 );
-
-        circle = mMap.addCircle( new CircleOptions()
-                .center( new LatLng( 6.2496173,-75.5695481 ) )
-                .radius( 100 )
-                .strokeWidth( 200 )
-                .strokeColor( colorStroke )
-                .fillColor( colorFill )
-                .clickable( true ) );
-
-        iconBit = getBitmapFromVectorDrawable( getContext(),drawerIcon );
-        mMap.addMarker( new MarkerOptions()
-                .icon(BitmapDescriptorFactory.fromBitmap( iconBit ))
-                .position( new LatLng(  6.2496173,-75.5695481 ) ).title( "Incident" ) );
-
-//        Cargara los datos dependiendo de la posicion donde se encuentre
-
-
-  //      Cargar las alertas sercanas en un rango mas grande
-
-    }
-    public Circle addCircle(String post) {
-
-        switch (post) {
-            case "Incident": colorStroke = Color.argb( 87,255,68,68 );  break;
-            case "Alert":  colorStroke = Color.argb( 63,116,133,227 );   break;
-        }
-        return circle;
-
-    }
-
-    public static Bitmap getBitmapFromVectorDrawable(Context context, int drawableId) {
-        Drawable drawable = ContextCompat.getDrawable(context, R.drawable.ic_add_location_24dp);
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            drawable = (DrawableCompat.wrap(drawable)).mutate();
-        }
-
-        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
-                drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-        drawable.draw(canvas);
-
-        return bitmap;
-    }
-
-    private void showAlert() {
-        final AlertDialog.Builder dialog = new AlertDialog.Builder( getContext() );
-        dialog.setTitle( "Enable Location" )
-                .setMessage( "Su ubicación esta desactivada.\npor favor active su ubicación " +
-                        "usa esta app" )
-                .setPositiveButton( "Configuración de ubicación", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Intent mi = new Intent( Settings.ACTION_LOCATION_SOURCE_SETTINGS );
-                        startActivity( mi );
-                    }
-                } )
-                .setNegativeButton( "Cancelar", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
-                    }
-                } );
-        dialog.show();
     }
 
 }
